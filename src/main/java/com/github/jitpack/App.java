8aaaa2c2fb26836ae24c76462a763dc56ccdadd9
PareshSwain <57855944+PareshSwain@users.noreply.github.com>
2019-12-09 19:10:46 +0530

    Update App.java

--- src/main/java/com/github/jitpack/App.java
+++ src/main/java/com/github/jitpack/App.java
@@ -6,6 +6,11 @@ package com.github.jitpack;
  */
 public class App 
 {
+Integer number1 = new Integer(123);
+Integer number2 = Integer.valueOf(123); 
+System.out.println("number1 =  " + number1);
+System.out.println("number2 =  " + number2);
+    
     public static void main(String[] args)
     {
         System.out.println(new App().greet("world"));
@@ -14,4 +19,6 @@ public class App
     public String greet(String name) {
         return "Hello " + name;
     }
+    
+
 }
